f9edfc8fbf4a7f5c22a46bd628f501fb2bb4709b
yashExploring <aggarwaly564@gmail.com>
2026-07-11 12:00:00 +0530

    [100] [Time Beats: 100.00%] [Memory Beats: 99.58%] - Leet2Hub

---
+++ 09-Trees/100. Same Tree/100. Same Tree.java
@@ -0,0 +1,22 @@
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSameTree(TreeNode p, TreeNode q) {
+18        if(p == null && q == null) return true;
+19        if(p == null || q == null) return false;
+20        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+21    }
+22}
