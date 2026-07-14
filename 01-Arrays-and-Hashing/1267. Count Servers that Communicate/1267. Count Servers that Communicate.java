b5fd6761760bd17da9d82c56149765c1089b3986
yashExploring <aggarwaly564@gmail.com>
2026-07-15 05:04:25 +0530

    [1267] [Time Beats: 99.40%] [Memory Beats: 12.35%] - Leet2Hub

--- 01-Arrays-and-Hashing/1267. Count Servers that Communicate/1267. Count Servers that Communicate.java
+++ 01-Arrays-and-Hashing/1267. Count Servers that Communicate/1267. Count Servers that Communicate.java
@@ -2,34 +2,33 @@
 2    public int countServers(int[][] grid) {
 3        int n = grid.length;
 4        int m = grid[0].length;
-5        
-6        int dC = 0;
-7        int totalCnt = 0;
-8        for(int i=0; i<n; i++) {
-9            int cnt = 0;
-10            int lJ = 0;
-11            for(int j=0; j<m; j++) {
-12                if(grid[i][j] == 1) {
-13                    lJ = j;
-14                    totalCnt++;
-15                    cnt++;
-16                }
-17            }
-18
-19            if(cnt == 1) {
-20                int nC = 0;
-21                for(int j=0; j<n; j++) {
-22                    if(grid[j][lJ] == 1) {
-23                        nC++;
-24                    }
-25                }
-26
-27                if(nC == 1) {
-28                    dC++;
-29                }
-30            }
-31        }
-32        
-33        return totalCnt-dC;
-34    }
-35}
+5        int totalCnt = 0;
+6        int isolatedCnt = 0;
+7        for(int i=0; i<n; i++) {
+8            int rowCnt = 0;
+9            int col = -1;
+10            for(int j=0; j<m; j++) {
+11                if(grid[i][j] == 1) {
+12                    totalCnt++; 
+13                    col = j;
+14                    rowCnt++;
+15                }
+16            }
+17
+18            int colCnt = 0;
+19            if(rowCnt == 1) {
+20                for(int x=0; x<n; x++) {
+21                    if(grid[x][col] == 1) {
+22                        colCnt++;
+23                    }
+24                }
+25
+26                if(colCnt == 1) {
+27                    isolatedCnt++;
+28                }
+29            }
+30        }
+31
+32        return totalCnt - isolatedCnt;
+33    }
+34}
